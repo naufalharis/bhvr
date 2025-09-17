adecf6cc962c3c2ae15a25f8edcc10759960eb11
M Afriza Rahman <muhamadafrizarahman@smkwikrama.sch.id>
2025-09-17 13:20:39 +0700

    Perubahan di file2

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./components/login";
+import Login from "./components/Login"; // buat komponen Login
+import Home from "./components/Home"; // buat komponen Home
 import "./index.css";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <Routes>
+      {/* Default route */}
+      <Route
+        path="/"
+        element={
+          isLoggedIn ? <Navigate to="/home" replace /> : <Navigate to="/login" replace />
+        }
+      />
+
+      {/* Login */}
+      <Route
+        path="/login"
+        element={
+          isLoggedIn ? <Navigate to="/home" replace /> : <Login onLogin={() => setIsLoggedIn(true)} />
+        }
+      />
+
+      {/* Home */}
+      <Route
+        path="/home"
+        element={isLoggedIn ? <Home /> : <Navigate to="/login" replace />}
+      />
+    </Routes>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        {/* Default arahkan ke halaman login */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
