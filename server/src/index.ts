7fc0d360133b8256674f1869c65092b170f9f191
naufalharis <naufalharis1012@gmail.com>
2025-09-17 16:10:47 +0700

    controller

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@
 import { Hono } from 'hono'
 import { createUser } from './controller/userController'
 import { loginUser } from './controller/loginController'
-import { createCourse } from './controller/courseController'
+import { createCourse, addChapter, addChapterContent } from './controller/courseController'
 import { authMiddleware } from "./middleware/authMiddleware";
 
 const app = new Hono()
@@ -11,5 +11,7 @@ app.get('/', (c) => c.text('Hello from Hono + Bun!'))
 app.post('/users', createUser) // langsung pakai handler Hono
 app.post('/login', loginUser);
 app.post("/courses", authMiddleware, createCourse);
+app.post("/courses/:courseId/chapters", authMiddleware, addChapter);
+app.post("/chapters/:chapterId/contents", authMiddleware, addChapterContent);
 
 export default app
