0efb824c473233e8de49ba51b0ff49e45cfee1d5
naufalharis <naufalharis1012@gmail.com>
2025-09-19 10:59:50 +0700

    kiwkiw

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import "../index.css";
+import React, { useEffect, useState } from "react";
+import "../styles/home.css";
 
 interface AppProps {
   onLogout: () => void;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
 // index.tsx
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./components/Login"; // buat komponen Login
-import Home from "./components/Home"; // buat komponen Home
-import "./index.css";
+import { BrowserRouter, Routes, Route, useNavigate, Navigate } from "react-router-dom";
+import Login from "./components/login";
+import Home from "./components/Home";
+import Register from "./components/Register";
+
+
 
 function App() {
   // Ambil status login dari localStorage
